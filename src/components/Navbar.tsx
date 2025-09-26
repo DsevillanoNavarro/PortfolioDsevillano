d81cddde457bc9f5c5d1ec76e560348c7f423803
Dsevillanonavarro <dsevillanonavarro123@gmail.com>
2025-09-26 13:03:33 +0200

    NavBar animation done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
-
+import { motion } from "framer-motion"
 function App() {
 
   return (
     <>
-      <div className="fixed  inset-0 flex flex-col justify-end items-center text-center px-4 py-4">
-        <div className="bg-blue-800 rounded flex ">
+    
+      <div className="fixed z-10 flex flex-col justify-end items-center text-center bottom-0 left-0 right-0 px-4 py-4">
+        <motion.div 
+        initial={{ opacity:0, y:-30}}
+        animate={{ opacity:1, y:0}}
+        transition={{ duration:0.8, ease: "easeOut"}}
+        className="bg-blue-800 rounded shadow-lg flex gap-6">
             <div>Hola</div>
             <div>Hola</div>
             <div>Hola</div>
             <div>Hola</div>
-        </div>
+        </motion.div>
         
       </div>
     </>
